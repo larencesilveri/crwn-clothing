9a76cea2e243af72582a6a03d8521636bf1389ec
larence <larencesilveri@gmail.com>
2020-05-26 15:04:11 +0530

    added redux, and implemented userReducer and userActions to header and app components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 import './App.scss';
 import Header from './components/header/header.component';
 import Home from './pages/home/home.component';
@@ -7,30 +9,24 @@ import Shop from './pages/shop/shop.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+import { setCurrentUser } from './redux/user/user.actions';
 
-    this.state = {
-      currentUser: null,
-    };
-  }
+class App extends Component {
   unsubscribeFromAuth = null;
   componentDidMount() {
+    const { setCurrentUser } = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot((snapShot) => {
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data(),
-            },
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data(),
           });
         });
       } else {
-        this.setState({ currentUser: userAuth });
+        setCurrentUser(userAuth);
       }
     });
   }
@@ -41,15 +37,17 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/shop" component={Shop} />
-          <Route exact path="/signin" component={SignInAndSignUp} />
+          <Route exact path='/' component={Home} />
+          <Route exact path='/shop' component={Shop} />
+          <Route exact path='/signin' component={SignInAndSignUp} />
         </Switch>
       </div>
     );
   }
 }
-
-export default App;
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
+export default connect(null, mapDispatchToProps)(App);
---
+++ src/redux/user/user.reducer.js
@@ -0,0 +1,19 @@
+import userActionTypes from './user.types';
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
+const userReducer = (state = INITIAL_STATE, { type, payload }) => {
+  switch (type) {
+    case userActionTypes.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
